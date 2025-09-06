0efb1b23a1f42f5ac96aedd894aa15ea79b9f591
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2025-09-06 19:47:11 +0100

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+// next.config.js
+const nextConfig = {
+  productionBrowserSourceMaps: true, // ✅ keep source maps in production
+};
+   //module.exports = nextConfig;
+export default nextConfig;
